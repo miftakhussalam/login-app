f69af4dfc63216b5ad86cfe48c8c0f61ea3d5054
mifta.salam <smiftakhus@gmail.com>
2025-10-22 07:54:57 +0700

    feat: implement rate limit to user signin

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const rateLimit = require("express-rate-limit");
+
+const loginLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 5,
+  message: {
+    status: "fail",
+    message: "Too many login attempts. Please try again in 1 minute.",
+  },
+});
+
 const {
   UserSignup,
   UserSignIn,
@@ -13,7 +24,7 @@ const { authenticateJWT } = require('../controller/verifyToken');
 const router = express.Router();
 
 router.post('/signup', UserSignup);
-router.post('/signin', UserSignIn);
+router.post('/signin', loginLimiter, UserSignIn);
 router.post('/verifyToken', UserVerifyToken);
 router.get('/', authenticateJWT, GetAllUser);
 router.get('/username/:username', authenticateJWT, GetUserByUsername);
